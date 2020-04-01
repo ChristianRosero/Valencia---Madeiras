cb772144729890a5f42ee627d2576ca2ed8a0bec
ChristianRosero <christianwork2010@gmail.com>
2020-04-01 23:57:54 +0100

    tries to finish

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,5 @@
+
+// --------------------------------- SLIDES HOMEPAGE -----------------
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -29,4 +31,38 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+}
+
+//---------------------- CART PAGE -----------------------
+// cart variables
+const IVA = 0.23;
+const shippingValue = 15.00; 
+let itemValue = document.getElementsByClassName('.subtotal');
+
+let iva = IVA * itemValue
+
+console.log = iva + shippingValue
+
+//--------------------- PRODUCTS PAGE ----------------
+
+const numItemsToGenerate = 20; //how many gallery items you want on the screen
+const numImagesAvailable = 242; //how many total images are in the collection you are pulling from
+const imageWidth = 480; //your desired image width in pixels
+const imageHeight = 480; //desired image height in pixels
+const collectionID = 1163637; //the collection ID from the original url
+const $galleryContainer = document.querySelector('.gallery-container');
+
+function renderGalleryItem(randomNumber){
+  fetch(`https://source.unsplash.com/collection/${collectionID}/${imageWidth}x${imageHeight}/?sig=${randomNumber}`) 
+    .then((response)=> {    
+      let galleryItem = document.createElement('div');
+      galleryItem.classList.add('gallery-item');
+      galleryItem.innerHTML = `<img class="gallery-image" src="${response.url}" alt="gallery image"/>`
+      $galleryContainer.appendChild(galleryItem);
+    })
+}
+
+for(let i=0;i<numItemsToGenerate;i++){
+  let randomImageIndex = Math.floor(Math.random() * numImagesAvailable);
+  renderGalleryItem(randomImageIndex);
 }
